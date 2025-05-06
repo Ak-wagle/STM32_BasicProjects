a065fe68195f5c732dff61481805ad667788131e
Ak-wagle <aniruddhawagle@gmail.com>
2025-05-06 12:50:07 +0530

    Update main README

--- Project_4_LED/Core/Src/main.c
+++ Project_4_LED/Core/Src/main.c
@@ -98,23 +98,20 @@ int main(void)
   while (1)
   {
     /* USER CODE END WHILE */
+
+    /* USER CODE BEGIN 3 */
 	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_SET);
 	  HAL_Delay(500);
 	  HAL_GPIO_WritePin(GPIOA, GPIO_PIN_1, GPIO_PIN_RESET);
-	 // HAL_Delay(100);
 	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_SET);
 	  HAL_Delay(500);
 	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_10, GPIO_PIN_RESET);
-	//  HAL_Delay(100);
 	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_SET);
 	  HAL_Delay(500);
 	  HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
-	 // HAL_Delay(100);
 	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_SET);
 	  HAL_Delay(500);
 	  HAL_GPIO_WritePin(GPIOC, GPIO_PIN_1, GPIO_PIN_RESET);
-	 // HAL_Delay(100);
-    /* USER CODE BEGIN 3 */
   }
   /* USER CODE END 3 */
 }
--- README.md
+++ README.md
@@ -1,2 +1,23 @@
+
 # STM32_BasicProjects
-STM32 Basic Projects
+
+This repository contains getting started guides, basic projects, and techniques for STM32 microcontrollers. Made for beginners and hobbyists!
+
+
+
+## Getting Started
+
+- [How To Setup New Project]()
+
+
+
+## Basic Projects
+
+- [LED Blink](https://github.com/Ak-wagle/STM32_BasicProjects/tree/main/Project_4_LED) 
+
+
+
+## tce. Board Projects
+
+- [Blink 4 LED](https://github.com/Ak-wagle/STM32_BasicProjects/tree/main/Project_4_LED) 
+
